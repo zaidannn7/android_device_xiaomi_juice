0d38eec645cbb2dd0bc99a8ff6a4f3463b30a6a3
Mahesh Sharma <smahesh@codeaurora.org>
2019-12-13 14:35:14 -0800

    juice: ANT: Support new ANT stack
    Add appropriate package name on the basis of
    transport used on target.
    
    Change-Id: I02163a7a50afba8c6ee6a766327b78c5df081d4e

--- device.mk
+++ device.mk
@@ -25,6 +25,11 @@ BOARD_SHIPPING_API_LEVEL := 29
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
