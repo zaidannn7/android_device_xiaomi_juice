70bd779ca0e66398ac9f7e285ef95d104957cdb9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:01:44 +0100

    juice: Build health HIDL
    Change-Id: I89c431c7ac87fe878537375cd811606d68f4bb1d

--- device.mk
+++ device.mk
@@ -152,6 +152,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Manufacturer
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.soc.manufacturer=QTI
