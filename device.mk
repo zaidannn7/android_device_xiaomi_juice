5e3a1713bdae2b44f0c6ca9fd22d06b8083d61ad
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:09:03 +0100

    juice: Build QTI USB HIDL
    Change-Id: Id7da15ac872967e4fb733836c99efaf39aca3a56

--- device.mk
+++ device.mk
@@ -305,6 +305,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.2-service-qti
+
 # Vendor property to enable advanced network scanning
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.enableadvancedscan=true
