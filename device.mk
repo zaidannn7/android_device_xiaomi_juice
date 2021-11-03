a66247b1e6824caa4da1054931c7a48f0dd52b60
Fabian Leuteneegger <fabian.leutenegger@bluewin.ch>
2021-11-03 17:53:48 +0100

    juice: Build Neural Networks HIDL
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-qti": library "android.hardware.neuralnetworks@1.0.so" not found: needed by main executable
    
    Change-Id: Ic6e1ea31f01c13fc6f2fa3dadf4bee672249f295

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
     netutils-wrapper-1.0
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
