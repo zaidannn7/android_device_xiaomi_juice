441065b11314cdd238d15fd7d00aaadda0287ab9
Alexey <lexaxx14@gmail.com>
2021-12-26 15:55:37 +0000

    juice: inital tree OSS
     * Redmi 9T/POCO M3
    
     * Follow configs from https://git.codelinaro.org/clo/la/platform/vendor/qcom/bengal/-/tree/LA.UM.9.15.r1-06500-KAMORTA.QSSI13.0
    
     * Credits - Demon000, ArianK16a, ZVNexus, erfanoabdi,
       imjyotiraditya, djchittoor, VishalCj, RE800T, gaganmalvi, Jabiyeff and others <3
    
    Signed-off-by: Alexey <lexaxx14@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_juice.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_juice-user \
+    lineage_juice-userdebug \
+    lineage_juice-eng
