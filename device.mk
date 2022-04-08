fd657243eda1ff523cba7b0e3e06b353e8e8f875
boedhack99 <boedhack@gmail.com>
2022-04-08 15:32:19 +0700

    juice: Import file system configs from CAF
        [1] - from LA.UM.9.15.r1-06500-KAMORTA.QSSI13.0

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1
 
+# Configs File System
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
