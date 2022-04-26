edfbe06a4478dce1d56dca064bbffd81a2a9da7f
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-26 03:11:50 +0000

    juice: Switch to overlay component
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -357,6 +357,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     gps \
     init \
+    overlay \
     usb \
     vibrator
 
@@ -465,7 +466,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
-    WifiResCommon \
+    JuiceWifiOverlay \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
