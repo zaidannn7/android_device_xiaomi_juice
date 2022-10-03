6698396b6be22ee15d161b5c65adeebd81547783
Alexey <lexaxx14@gmail.com>
2022-10-03 14:04:49 +0000

    juice: Import SurfaceFlinger props from raven
        [1] - https://github.com/GrapheneOS/device_google_gs101/blob/850bc164c59c5591a9ce33a04efba5e39c508183/device.mk#L651
    
    Signed-off-by: Alexey <lexaxx14@gmail.com>

--- device.mk
+++ device.mk
@@ -236,6 +236,35 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+# Graphics / from gs101
+PRODUCT_PROPERTY_OVERRIDES += \
+   debug.sf.disable_backpressure=0 \
+   debug.sf.enable_gl_backpressure=1
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+   debug.sf.use_phase_offsets_as_durations=1 \
+   debug.sf.late.sf.duration=10500000 \
+   debug.sf.late.app.duration=16600000 \
+   debug.sf.early.sf.duration=16600000 \
+   debug.sf.early.app.duration=16600000 \
+   debug.sf.earlyGl.sf.duration=16600000 \
+   debug.sf.earlyGl.app.duration=16600000 \
+   debug.sf.frame_rate_multiple_threshold=120 \
+   debug.sf.layer_caching_active_layer_timeout_ms=1000 \
+   debug.sf.treat_170m_as_sRGB=1
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+   ro.surface_flinger.enable_layer_caching=true \
+   ro.surface_flinger.set_idle_timer_ms?=80 \
+   ro.surface_flinger.set_touch_timer_ms=200 \
+   ro.surface_flinger.set_display_power_timer_ms=1000 \
+   ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+   ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+   ro.surface_flinger.supports_background_blur=1
+
+PRODUCT_SYSTEM_PROPERTIES += \
+   ro.launcher.blur.appLaunch=0
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
