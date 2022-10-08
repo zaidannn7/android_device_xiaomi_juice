96be5e5730e4b6813572004a0eb6b29790d29d09
Alexey <lexaxx14@gmail.com>
2022-10-08 06:41:54 +0000

    juice: Import framework compatibility matrix from MIUI V12.5.11.0.RJQMIXM
    Signed-off-by: Alexey <lexaxx14@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,8 @@ TARGET_OTA_ASSERT_DEVICE := lime,citrus,lemon,pomelo,juice
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_juice
