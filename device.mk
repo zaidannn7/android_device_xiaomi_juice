09147481701b82bba31962f265a40b57360525de
shelbyhell <lexaxx14@gmail.com>
2022-11-19 16:35:43 +0000

    juice: Import msm_irqbalance from device/qcom/common
        [1] - https://github.com/AOSPA/android_device_qcom_common/blob/topaz/vendor/perf/configs/bengal/msm_irqbalance.conf
    
    Signed-off-by: shelbyhell <lexaxx14@gmail.com>

--- device.mk
+++ device.mk
@@ -403,6 +403,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
