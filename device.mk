483db94d693661936fce6c9953f87daa73eab58a
vsc-sxx <shelbyhell@proton.me>
2022-11-21 17:55:28 +0000

    Revert "juice: Uprev bluetooth audio HIDL impl to 2.1"
    This reverts commit 6d83f000ffa9c894cf7d0be1a14539f8d97bf79e.

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.a2dp@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
