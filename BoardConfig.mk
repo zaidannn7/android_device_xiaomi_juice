62f87443c7c0ebf376015e25bc2909232d3922e2
vsc-sxx <shelbyhell@proton.me>
2022-11-21 02:30:12 +0000

    fixup! juice: sepolicy: go enforcing.
      [1] - Import sepolicy from spes and adapting.
    
    Signed-off-by: vsc-sxx <shelbyhell@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,8 +195,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble flag
 BOARD_VNDK_VERSION := current
